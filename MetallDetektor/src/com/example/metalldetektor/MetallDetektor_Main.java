b772e0bdfe380c6ef31a8b80a050f6a1c6083bb0
lucaipp <lucaippolito@hotmail.ch>
2014-09-20 14:42:40 +0200

    Update

--- MetallDetektor/src/com/example/metalldetektor/MetallDetektor_Main.java
+++ MetallDetektor/src/com/example/metalldetektor/MetallDetektor_Main.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MetallDetektor_Main extends Activity {
@@ -15,10 +17,17 @@ public class MetallDetektor_Main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metall_detektor_main);
         //Prüfen ob Sensor vorhanden ist
-        //SensorManager sm = SensorManager getSystemservice(Context.SENSOR_SERVICE);
-        		
-    }
+        
 
+        final Button btnStart = (Button) findViewById(R.id.btnStart);
+        final Button btnStop = (Button) findViewById(R.id.btnStop);
+        
+        btnStart.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+        	btnStop.setEnabled(true);
+    		}
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
